204f308106e917686c33d73c660f484f1ad78773
ctrindadedev <caiomedtrindade@gmail.com>
2025-02-25 23:02:52 -0300

    esqueleto

---
+++ gab1/src/App2.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+// Arrow funcion
+const Teste = () => <h1> Olá, mundo </h1>
+// rafce
+import React from 'react'
+
+const App2 = () => {
+  return (
+    <h1> Olá, mundo </h1>
+  )
+}
+
+export default App2
+
+
+//Nomeação de Componentes/Funções
+// PascalCase => Cachorro ; CachorroDalmata
